4fd1276b0569bb02f4ecbb53b6c8915abc5de3d5
Erik Hansen <erik@krakencommerce.com>
2019-11-07 14:33:42 -0600

    Prevent error due to PHP method return mismatch

--- Plugin/Model/Product/Type/GroupedPlugin.php
+++ Plugin/Model/Product/Type/GroupedPlugin.php
@@ -20,4 +20,24 @@ class GroupedPlugin
 
         return $result;
     }
+
+    /**
+     * Prevent error
+     *
+     * The self::afterGetAssociatedProductCollection
+     * code causes the \Magento\MsrpGroupedProduct\Pricing\MsrpPriceCalculator::getMsrpPriceValue method to be called,
+     * which in turn throws this error:
+     *
+     * PHP message: PHP Fatal error:  Uncaught TypeError: Return value of
+     * Magento\MsrpGroupedProduct\Pricing\MsrpPriceCalculator::getMsrpPriceValue() must be of the type float, string
+     * returned in vendor/magento/module-msrp-grouped-product/Pricing/MsrpPriceCalculator.php:35
+     *
+     * @param \Magento\GroupedProduct\Model\Product\Type\Grouped $subject
+     * @param $result
+     * @return float
+     */
+    public function afterGetChildrenMsrp(\Magento\GroupedProduct\Model\Product\Type\Grouped $subject, $result)
+    {
+        return (float)$result;
+    }
 }
