b02d19d2b1ca80405edbadd9c61f36f65aeab84b
Erik Hansen <erik@krakencommerce.com>
2019-11-08 11:48:29 -0600

    Hide the 'Click for price' link on tiered pricing

---
+++ Plugin/Pricing/Price/MsrpPricePlugin.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @category    Kraken
+ * @copyright   Copyright (c) 2019 Kraken, LLC
+ */
+namespace Kraken\DisplayMsrpInline\Plugin\Pricing\Price;
+
+class MsrpPricePlugin
+{
+    /**
+     * Disable the "Click for price" link from showing
+     *
+     * Even though the global configuration for "Display Actual Price" is "On Gesture" we are overriding
+     * it here so that the template files that call this method don't output the "Click for price" link (both
+     * for normal pricing display as well as for tiered pricing)
+     *
+     * @param \Magento\Msrp\Pricing\Price\MsrpPrice $subject
+     * @return bool
+     */
+    public function beforeIsShowPriceOnGesture(\Magento\Msrp\Pricing\Price\MsrpPrice $subject)
+    {
+        return false;
+    }
+}
